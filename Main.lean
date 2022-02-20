7fc98bb679622e9ce97ffa5d9b24764e2ae6cbf8
grekiki <gregor1234567890@gmail.com>
2022-02-20 12:04:29 +0100

    2/1 done

--- Main.lean
+++ Main.lean
@@ -2,9 +2,7 @@ import Solution01
 import Solution02
 
 def main : IO Unit := do
-  let s1 <- solvePart11 "Solved/Day01/input.txt"
-  IO.println s1;
-  let s2 <- solvePart12 "Solved/Day01/input.txt"
-  IO.println s2;
+  let s1 <- solvePart21 "Solved/Day02/input.txt"
+  IO.println s1
 
--- #eval main
+#eval main
--- Solved/Day02/Solution02.lean
+++ Solved/Day02/Solution02.lean
@@ -1,5 +1,25 @@
-def a := "This is problem 2"
 
-def solvePart21 : IO Unit :=
-  IO.println s!"Hello, {a}!"
+def parse(cmd:String)(n:Int):(Int × Int) :=
+  if cmd=="forward" then
+    (n, 0)
+  else if cmd=="down" then
+    (0, n)
+  else
+    (0, -n)
 
+def parseCommands(comds:List String)(pos:Int × Int) : (Int × Int) :=
+  match comds with
+  | [] => pos
+  | h::t => 
+    match String.splitOn h with
+    | [cmd, num] => let (x, y) := parse cmd (String.toInt! num)
+                    parseCommands t (pos.fst+x, pos.snd+y)
+    | _ => panic "Split failure"
+
+
+def solvePart21 (s : String) : IO String := do
+    let input ← IO.FS.lines s
+    let pos := parseCommands input.toList (0,0)
+    return s!"{pos.fst*pos.snd}"
+
+#eval solvePart21 "Solved/Day02/small.txt"
