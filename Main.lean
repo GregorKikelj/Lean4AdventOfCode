080d4ae9ed51e802d52f3441a6b54b5bcc057dbf
grekiki <gregor1234567890@gmail.com>
2022-02-21 00:34:21 +0100

    day2 done

--- Main.lean
+++ Main.lean
@@ -4,5 +4,7 @@ import Solution02
 def main : IO Unit := do
   let s1 <- solvePart21 "Solved/Day02/input.txt"
   IO.println s1
+  let s2 <- solvePart22 "Solved/Day02/input.txt"
+  IO.println s2
 
 #eval main
--- Solved/Day02/Solution02.lean
+++ Solved/Day02/Solution02.lean
@@ -22,4 +22,30 @@ def solvePart21 (s : String) : IO String := do
     let pos := parseCommands input.toList (0,0)
     return s!"{pos.fst*pos.snd}"
 
-#eval solvePart21 "Solved/Day02/small.txt"
+
+-- pair of aim and pos
+def parse2(cmd:String)(n:Int)(aim:Int)(pos: Int × Int):Int × (Int × Int):=
+  if cmd=="forward" then
+    (aim, (pos.fst+n, pos.snd+n*aim)) 
+  else if cmd=="down" then
+    (aim+n, pos)
+  else
+    (aim-n, pos)
+
+
+def parseCommands2(cmds:List String)(aim:Int)(pos: Int × Int): (Int × Int) :=
+  match cmds with
+  |[] => pos
+  |h::t => 
+    match String.splitOn h with
+    | [cmd, num] => let (aim2, pos2):= parse2 cmd (String.toInt! num) aim pos
+                    parseCommands2 t aim2 pos2
+    | _ => panic "Split2 failure"
+
+def solvePart22 (s : String) : IO String := do
+    let input ← IO.FS.lines s
+    let pos := parseCommands2 input.toList 0 (0,0)
+    return s!"{pos.fst*pos.snd}"
+
+
+
